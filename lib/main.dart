450de4f11180e4336ac441a14b6ae293686cc7d1
Eldringrg <grg.aldrin@ymail.com>
2023-01-06 15:33:04 +0545

    Id

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,45 @@ class idCard extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('ID Card'),
+          title: Container(
+            width: double.infinity,
+            child: Text(
+              'ID-Card',
+              textAlign: TextAlign.center,
+            ),
+          ),
         ),
-        body: Card(
-          child: Text('Hello'),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(20),
+            child: Card(
+              borderOnForeground: true,
+              elevation: 20,
+              child: Column(
+                children: <Widget>[
+                  Image.asset(
+                    'assests/PAS075BCT019.jpg',
+                  ),
+                  Text(
+                    'ELDRIN GURUNG',
+                    textScaleFactor: 1.5,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'PAS075BCT019',
+                    textScaleFactor: 1.5,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'Computer Engineering',
+                    textScaleFactor: 1.5,
+                    style: TextStyle(
+                        color: Colors.green, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
