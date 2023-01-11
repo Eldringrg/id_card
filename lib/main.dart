502462bc347647c2bbe360293d35fa1efc6bab59
Eldringrg <grg.aldrin@ymail.com>
2023-01-11 19:13:44 +0545

    themeing

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,10 @@ class idCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: Container(
@@ -24,28 +28,31 @@ class idCard extends StatelessWidget {
             child: Card(
               borderOnForeground: true,
               elevation: 20,
-              child: Column(
-                children: <Widget>[
-                  Image.asset(
-                    'assests/PAS075BCT019.jpg',
-                  ),
-                  Text(
-                    'ELDRIN GURUNG',
-                    textScaleFactor: 1.5,
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    'PAS075BCT019',
-                    textScaleFactor: 1.5,
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    'Computer Engineering',
-                    textScaleFactor: 1.5,
-                    style: TextStyle(
-                        color: Colors.green, fontWeight: FontWeight.bold),
-                  ),
-                ],
+              child: Container(
+                padding: EdgeInsets.all(30),
+                child: Column(
+                  children: <Widget>[
+                    Image.asset(
+                      'assests/PAS075BCT019.jpg',
+                    ),
+                    Text(
+                      'ELDRIN GURUNG',
+                      textScaleFactor: 1.5,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'PAS075BCT019',
+                      textScaleFactor: 1.5,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Computer Engineering',
+                      textScaleFactor: 1.5,
+                      style: TextStyle(
+                          color: Colors.green, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
